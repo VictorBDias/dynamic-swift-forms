901870edc4f89eebc2d1ff1e2c3f0206a23790b5
Victor Batisttete Dias <victor.batisttete@yazo.com.br>
2025-02-16 15:40:46 -0300

    otimization with lazyStack

--- cloud-evaluation/View/FormDetailView.swift
+++ cloud-evaluation/View/FormDetailView.swift
@@ -6,7 +6,6 @@
 //
 
 
-
 import SwiftUI
 import WebKit
 
@@ -18,25 +17,27 @@ struct FormDetailView: View {
     @State private var formData: [String: String] = [:]
 
     var body: some View {
-        Form {
-            ForEach(form.sectionsArray, id: \.uuid) { section in
-                Section(header: VStack(alignment: .leading) {
-                    WebView(html: section.title ?? "Section")
-                        .frame(height: 100)
-                }) {
-                    let sectionFields = form.fieldsArray.filter { field in
-                        guard let fieldIndex = form.fieldsArray.firstIndex(where: { $0.uuid == field.uuid }) else {
-                            return false
+        ScrollView {
+            LazyVStack(alignment: .leading) { // Align all content to the leading edge
+                ForEach(form.sectionsArray, id: \.uuid) { section in
+                    Section(header: VStack(alignment: .leading) {
+                        WebView(html: section.title ?? "Section")
+                            .frame(height: 100)
+                    }) {
+                        let sectionFields = form.fieldsArray.filter { field in
+                            guard let fieldIndex = form.fieldsArray.firstIndex(where: { $0.uuid == field.uuid }) else {
+                                return false
+                            }
+                            return fieldIndex >= section.from && fieldIndex <= section.to
                         }
-                        return fieldIndex >= section.from && fieldIndex <= section.to
-                    }
 
-                    ForEach(sectionFields, id: \.uuid) { field in
-                        getFieldView(for: field)
-
+                        ForEach(sectionFields, id: \.uuid) { field in
+                            getFieldView(for: field)
+                        }
                     }
                 }
             }
+            .padding()
         }
         .navigationTitle("")
         .toolbar {
@@ -44,7 +45,6 @@ struct FormDetailView: View {
                 Text("Fill Form Entry")
                     .font(.system(size: 16, weight: .semibold))
             }
-
 
             ToolbarItem(placement: .navigationBarTrailing) {
                 Button("Save", action: saveEntry)
@@ -59,85 +59,91 @@ struct FormDetailView: View {
     private func getFieldView(for field: FieldEntity) -> some View {
         switch field.type {
         case "number":
-            Text(field.label ?? "Label")
-                .foregroundColor(.gray)
-
-            Stepper(
-                value: Binding(
-                    get: { Int(formData[field.uuid ?? ""] ?? "0") ?? 0 },
-                    set: { formData[field.uuid ?? ""] = String($0) }
-                ),
-                in: 0...100
-            ) {
-                Text("\(formData[field.uuid ?? ""] ?? "0")")
+            VStack(alignment: .leading) { // Align content to the leading edge
+                Text(field.label ?? "Label")
                     .foregroundColor(.gray)
-                    .padding()
-                    .background(Color.gray.opacity(0.1))
-                    .cornerRadius(5)
+
+                Stepper(
+                    value: Binding(
+                        get: { Int(formData[field.uuid ?? ""] ?? "0") ?? 0 },
+                        set: { formData[field.uuid ?? ""] = String($0) }
+                    ),
+                    in: 0...100
+                ) {
+                    Text("\(formData[field.uuid ?? ""] ?? "0")")
+                        .foregroundColor(.gray)
+                        .padding()
+                        .background(Color.gray.opacity(0.1))
+                        .cornerRadius(5)
+                }
             }
 
         case "dropdown":
-            Text(field.label ?? "Label")
-                .foregroundColor(.gray)
+            VStack(alignment: .leading) { // Align content to the leading edge
+                Text(field.label ?? "Label")
+                    .foregroundColor(.gray)
 
-            if !field.optionsArray.isEmpty {
-                let defaultSelection = formData[field.uuid ?? ""] ?? field.optionsArray.first?.value ?? ""
+                if !field.optionsArray.isEmpty {
+                    let defaultSelection = formData[field.uuid ?? ""] ?? field.optionsArray.first?.value ?? ""
 
-                HStack {
-                    Picker(selection: Binding(
-                        get: { formData[field.uuid ?? ""] ?? defaultSelection },
-                        set: { formData[field.uuid ?? ""] = $0 }
-                    ), label: EmptyView()) {
-                        ForEach(field.optionsArray, id: \.value) { option in
-                            Text(option.label)
-                                .tag(option.value)
-                                .padding(.vertical, 10)
+                    HStack {
+                        Picker(selection: Binding(
+                            get: { formData[field.uuid ?? ""] ?? defaultSelection },
+                            set: { formData[field.uuid ?? ""] = $0 }
+                        ), label: EmptyView()) {
+                            ForEach(field.optionsArray, id: \.value) { option in
+                                Text(option.label)
+                                    .tag(option.value)
+                                    .padding(.vertical, 10)
+                            }
                         }
-                    }
-                    .pickerStyle(MenuPickerStyle())
-                    .frame(height: 50)
-                    .cornerRadius(5)
+                        .pickerStyle(MenuPickerStyle())
+                        .frame(height: 50)
+                        .cornerRadius(5)
 
-                    Spacer()
+                        Spacer()
+                    }
+                    .frame(height: 50)
+                    .padding(.horizontal, 10)
+                    .background(Color.gray.opacity(0.1))
+                    .cornerRadius(5)
+                } else {
+                    Text("No options available")
+                        .foregroundColor(.red)
                 }
-                .frame(height: 50)
-                .padding(.horizontal, 10)
-                .background(Color.gray.opacity(0.1))
-                .cornerRadius(5)
-            } else {
-                Text("No options available")
-                    .foregroundColor(.red)
             }
 
-
         case "description":
-            WebView(html: field.label ?? "Description")
-            .frame(height: 40)
+            VStack(alignment: .leading) { // Align content to the leading edge
+                WebView(html: field.label ?? "Description")
+                    .frame(height: 40)
 
-            TextField("Fill the field", text: Binding(
-                get: { formData[field.uuid ?? ""] ?? "" },
-                set: { formData[field.uuid ?? ""] = $0 }
-            ))
-            .padding()
-            .background(Color.gray.opacity(0.1))
-            .cornerRadius(5)
-            .foregroundColor(.gray)
+                TextField("Fill the field", text: Binding(
+                    get: { formData[field.uuid ?? ""] ?? "" },
+                    set: { formData[field.uuid ?? ""] = $0 }
+                ))
+                .padding()
+                .background(Color.gray.opacity(0.1))
+                .cornerRadius(5)
+                .foregroundColor(.gray)
+            }
 
         default:
-            Text(field.label ?? "Label")
-                .foregroundColor(.gray)
+            VStack(alignment: .leading) { // Align content to the leading edge
+                Text(field.label ?? "Label")
+                    .foregroundColor(.gray)
 
-            TextField("Enter \(field.label ?? "text")", text: Binding(
-                get: { formData[field.uuid ?? ""] ?? "" },
-                set: { formData[field.uuid ?? ""] = $0 }
-            ))
-            .padding()
-            .background(Color.gray.opacity(0.1))
-            .cornerRadius(5)
-            .foregroundColor(.gray)
+                TextField("Enter \(field.label ?? "text")", text: Binding(
+                    get: { formData[field.uuid ?? ""] ?? "" },
+                    set: { formData[field.uuid ?? ""] = $0 }
+                ))
+                .padding()
+                .background(Color.gray.opacity(0.1))
+                .cornerRadius(5)
+                .foregroundColor(.gray)
+            }
         }
     }
-
 
     /// Load existing data from Core Data
     private func loadExistingData() {
